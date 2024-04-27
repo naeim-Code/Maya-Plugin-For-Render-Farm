0e3cad03ea6e0da1e81085b40c5f2a1bd15ee8fb
Naeim Akbari <79456673+naeim-Code@users.noreply.github.com>
2024-04-28 00:29:49 +0330

    farm.py

---
+++ farm.py
@@ -0,0 +1,33 @@
+import maya.cmds as cmds
+import sys 
+import farmWin  
+
+def shelfCheckScene():
+    farmWin.shelfCheckScene()
+ 
+def shelfPreUpload():
+    farmWin.shelfPreUpload() 
+
+def shelfRenderNow():
+    farmWin.shelfRenderNow() 
+
+def shelfRenderSetup():
+    farmWin.shelfRenderSetup() 
+
+def shelfControlCenter():
+    farmWin.shelfControlCenter() 
+
+def shelfCostCalculator():
+    farmWin.shelfCostCalculator()
+ 
+def shelfCostEstimation():
+    farmWin.shelfCostEstimation() 
+
+def shelfRenderOutput():
+    farmWin.shelfRenderOutput()
+   
+def shelfSkyHome():
+    farmWin.show_window()
+
+def init():
+    farmWin.init()
